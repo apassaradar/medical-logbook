e90efa4202dcb6d5064c04a8f91813bf59bf2024
apassaradar <97677911+apassaradar@users.noreply.github.com>
2022-03-20 01:36:10 +0700

    Revert "Feature/login"

--- server/seeders/20220307153711-demo-courses.js
+++ server/seeders/20220307153711-demo-courses.js
@@ -4,43 +4,30 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.bulkInsert('course', [{
-       id:'1',
-       name: 'patients',
-    }, {
-        id:'2',
+      name: 'patients',
+    },{
       name: 'opd',
-       }, {
-          id:'3',
+      }, {
         name:'conferance',
-       }, {
-          id:'4',
+      }, {
          name:'emergency',
-       }, {
-          id:'5',
+      }, {
          name:'conferance',
-       }, {
-          id:'6',
+      }, {
          name:'observemajor',
-       }, {
-          id:'7',
+      }, {
          name:'helpmajor',
-       }, {
-          id:'8',
+      }, {
          name:'helpobserveminor',
-       }, {
-          id:'9',
+      }, {
          name:'firstaid',
-       }, {
-          id:'10',
+      },{
          name:'stitches',
-       }, {
-          id:'11',
+      },{
          name:'foleycath',
-       }, {
-          id:'12',
+      }, {
          name:'cvp',
-       }, {
-          id:'13',
+      },{
          name:'resident',
       },
     ])
--- server/seeders/20220307184816-demo-user.js
+++ server/seeders/20220307184816-demo-user.js
@@ -1,38 +1,17 @@
 'use strict';
-const bcrypt = require("bcrypt");
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const date = new Date();
-    const hash =await bcrypt.hash('123456',10)
+  up: (queryInterface, Sequelize) => {
+
     return queryInterface.bulkInsert('users', [{
-       id:'1',
       fname: 'Artid',
       lname: 'Sudjoi',
       email: 'artid@gmail.com',
-      password:hash,
+      password:'123456',
       roleID: '1',
-      createdAt: date,
-      updatedAt: date,
-    }, {
-        id:'2',
-      fname: 'Artid',
-      lname: 'Sudjoi',
-      email: 'game@gmail.com',
-      password:hash,
-      roleID: '2',
-      createdAt: date,
-      updatedAt: date,
-      }, {
-      id:'3',
-      fname: 'Artid',
-      lname: 'Sudjoi',
-      email: 'dar@gmail.com',
-      password:hash,
-      roleID: '3',
-      createdAt: date,
-      updatedAt: date,
-      
-    },
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, 
   
     ])
   },
